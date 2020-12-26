19e74cf8135efe1816fab8563113ca889fa6124b
Stefan Kerschbaumer <kerschbaumer@lean-coders.at>
2020-12-26 09:36:46 +0100

    feat(nest-modules): makes userservice public in controller

--- src/base-user/base-user.controller.ts
+++ src/base-user/base-user.controller.ts
@@ -1,10 +1,12 @@
 import { Body, Controller, Post } from "@nestjs/common";
+
+import { ApiResponse } from "@nestjs/swagger";
 import { BaseUserService } from "./base-user.service";
 import { BaseUserModel } from "./models";
 
 @Controller("user")
 export class BaseUserController<T = BaseUserModel> {
-  constructor(private userService: BaseUserService<T>) {}
+  constructor(public userService: BaseUserService<T>) {}
 
   @Post("/create")
   private async createUser(@Body() user: T) {
