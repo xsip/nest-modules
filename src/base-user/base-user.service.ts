6384ff140295c423c24811790e522225770f9c98
Stefan Kerschbaumer <kerschbaumer@lean-coders.at>
2020-12-27 12:59:25 +0100

    feat(nestmodules): adds model repo

--- src/base-user/base-user.service.ts
+++ src/base-user/base-user.service.ts
@@ -3,23 +3,26 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Document, Model } from 'mongoose';
 import { BaseUserModel } from './models/base-user.model';
 import * as bcrypt from 'bcrypt';
+import { ModelRepo } from '../core/services/model.repo';
 
 @Injectable()
-export class BaseUserService<T = BaseUserModel> {
+export class BaseUserService<T = BaseUserModel> extends ModelRepo<T> {
   constructor(
     @InjectModel('user') public userModel: Model<T & Document>,
-  ) {}
+  ) {
+    super(userModel);
+  }
 
   async createUser(user: T): Promise<T> {
-    return this.userModel.create({
+    return this.create({
       ...(user as any),
       password: this.hashPassword(user['password']),
-    } as T & Document);
+    });
   }
 
   public async findByEmail(email: string) {
     try {
-      return await this.userModel.findOne({ email: email } as any);
+      return await this.findOne({ email: email } as any);
     } catch (e) {
       throw new Error(`Couldn't find user with email "${email}"`);
     }
---
+++ src/core/services/model.repo.ts
@@ -0,0 +1,21 @@
+import { Document, FilterQuery, Model, ObjectId } from 'mongoose';
+
+export class ModelRepo<T> {
+  constructor(public model: Model<T & Document>) {}
+
+  async findOne(query: Partial<T> | FilterQuery<T>): Promise<T> {
+    return this.model.findOne(query as any).exec();
+  }
+
+  async findMany(query: FilterQuery<T>): Promise<T[]> {
+    return this.model.find(query as any).exec();
+  }
+
+  async updateOne(id: ObjectId, model: T): Promise<T> {
+    return this.model.findByIdAndUpdate(id, model).exec();
+  }
+
+  async create(model: T & Document): Promise<T> {
+    return this.model.create(model as T & Document);
+  }
+}
